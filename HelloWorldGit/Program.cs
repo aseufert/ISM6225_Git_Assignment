a02e395cdc57bbdba90d5843af2b2f545a9ca302
Alex Seufert <amseufert@gmail.com>
2020-06-20 14:33:19 -0400

    added HelloFromAlex method

--- HelloWorldGit/Program.cs
+++ HelloWorldGit/Program.cs
@@ -4,9 +4,13 @@ namespace HelloWorldGit
 {
     class Program
     {
+        static void HelloFromAlex()
+        {
+            Console.WriteLine("Alex says hello world!");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HelloFromAlex();
         }
     }
 }
